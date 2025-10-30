fb508753a80607d357ee68d5667eae946cb0b74c
MoozIiSP <yuaolni@gmail.com>
2025-10-31 01:41:09 +0800

    fix: 修复主题

--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -47,7 +47,7 @@ sbw_pkg_repo="https://github.com/sbwml/openwrt_pkgs"
 natmap_repo="https://github.com/blueberry-pie-11/luci-app-natmap"
 xwrt_repo="https://github.com/QiuSimons/openwrt-natflow"
 tailscale_repo="https://github.com/asvow/luci-app-tailscale"
-luci-theme-design="https://github.com/SAENE/luci-theme-design"
+luci_theme_design_repo="https://github.com/SAENE/luci-theme-design"
 
 # 开始克隆仓库，并行执行
 clone_repo $openwrt_repo $latest_release openwrt &
@@ -63,7 +63,7 @@ clone_repo $openwrt_pkg_repo master openwrt_pkg_ma &
 clone_repo $openwrt_add_repo master OpenWrt-Add &
 clone_repo $dockerman_repo master dockerman &
 clone_repo $docker_lib_repo master docker_lib &
-clone_repo $luci-theme-design master luci-theme-design &
+clone_repo $luci_theme_design_repo master luci_theme_design_repo &
 # 等待所有后台任务完成
 wait
 
